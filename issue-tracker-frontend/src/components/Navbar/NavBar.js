549046c7e2250153ba09e5a74cf2a85f02c98e46
Seno <ryne0013@hotmail.com>
2021-06-27 14:16:22 -0500

    Styled Navigation Bar
    Implemented the CSS for the navigation bar and links to other screens.

--- issue-tracker-frontend/src/components/Navbar/NavBar.js
+++ issue-tracker-frontend/src/components/Navbar/NavBar.js
@@ -1,22 +1,29 @@
 import React, {Component } from "react";
 import {MenuItems} from "./MenuItems";
 import teamLogo from "../../Images/snakeLogo.png";
-import './NavBar.css';
+import './NavBar.css'
+import {NavButton} from '../NavButton';
 
 class Navbar extends Component {
     state = { clicked: false}
     
+    handleClick = () => {
+        this.setState({ clicked:!this.state.clicked })
+    }
     render() {
         return(
             <nav className='NavBarItems'>
-                <h1 className="navbar-title" color="green">Team Mamba's Issue Tracker</h1>
+                <h1 className="navbar-logo" color="green">
+                    Team Mamba's Issue Tracker 
+                    <i className="fas fa-dragon"></i>
+                </h1>
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
-                <ul>
+                <ul className={this.state.clicked ? 'nav-menu-active' : 'nav-menu'}>
                     {MenuItems.map((item, index) => {
                         return (
-                            <li> key={index}
+                            <li key={index}> 
                                 <a className={item.cName} href={item.url}>
                                     {item.title}
                                 </a>
@@ -24,6 +31,7 @@ class Navbar extends Component {
                         )
                     })}
                 </ul>
+                <NavButton>Sign Up</NavButton>
             </nav>
         )
     }
